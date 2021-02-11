5b8d23a2bc88e342455e11600c4bbd925f93c8f6
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2021-02-11 21:53:12 +0100

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "http://localhost:8080/dist/",
+  },
+};
