2420171737b3072d997d79d7756ede786bde4a40
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2021-02-12 07:21:13 +0100

    Install and configure prettier to work with eslint
    Add the format npm command to format all the project files.
    Format the project.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const App: React.FC = () => (
   <div className="App">
     <h1> Hello, World! </h1>
   </div>
-);
+)
 
-export default App;
+export default App
