5ca5a6a92566486c2b3c4c6475196a436693ba28
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2021-02-16 07:16:58 +0100

    Allow Jest to calculate the coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  rootDir: './src',
+  roots: ['<rootDir>/src/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src'],
+  coverageDirectory: '<rootDir>/coverage',
 }
